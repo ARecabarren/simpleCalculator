d710585c0bc5956e1d90070199fdee876144247f
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-03-16 16:59:19 -0300

    Add operate function and code for clear button

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Basic functions
+
 function add(a,b){
     return a+b
 }
@@ -10,3 +12,31 @@ function multiply(a,b) {
 function divide(a,b){
     return a/b
 }
+function operate(a,b,operator) {
+    let result;
+    switch(operator){
+        case '+':
+            result = add(a,b);
+            break;
+        case '-':
+            result = subtract(a,b);
+            break;
+        case '*':
+            result = multiply(a,b);
+            break;
+        case '/':
+            result = divide(a,b);
+            break;
+        default:
+            result = 'Invalid operator';
+            break
+    } return result
+}
+
+
+let calculatorDisplay = document.querySelector('.calculator-display')
+
+let clearButton = document.querySelector('.button-clear');
+clearButton.addEventListener('click',() => {
+    calculatorDisplay.textContent = ""
+} )
