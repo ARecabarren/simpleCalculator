c3b31b772bc165c367b779e51680b8acb6fb5ad6
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-03-18 20:53:21 -0300

    Fix consecutive operations control

--- script.js
+++ script.js
@@ -33,10 +33,69 @@ function operate(a,b,operator) {
     } return result
 }
 
-
 let calculatorDisplay = document.querySelector('.calculator-display')
 
 let clearButton = document.querySelector('.button-clear');
 clearButton.addEventListener('click',() => {
-    calculatorDisplay.textContent = ""
-} )
+    calculatorDisplay.textContent = "";
+    displayValue = ''
+    firstValue = undefined;
+    secondValue = undefined;
+    currentOperator = undefined;
+})
+
+let displayValue = ''
+//Populate display when click buttons
+
+let currentOperator,
+    lastOperator,
+    decimalOff = true,
+    firstValue,
+    secondValue;
+
+numbersButtons = document.querySelectorAll('.button-number');
+
+for (const button of numbersButtons) {
+    button.addEventListener('click', ()=>{
+        if(!currentOperator || !firstValue){
+            calculatorDisplay.textContent += button.value
+        }else if (firstValue && calculatorDisplay.textContent !== '') {
+            calculatorDisplay.textContent = ''
+            // currentOperator = undefined
+            calculatorDisplay.textContent += button.value
+        }else{
+            calculatorDisplay.textContent += button.value
+        }
+
+    })
+}
+
+operatorsButtons = document.querySelectorAll('.button-operator')
+for (const button of operatorsButtons) {
+    button.addEventListener('click', () =>{
+        if(!currentOperator){
+            currentOperator = button.value
+        }else{
+            lastOperator = currentOperator;
+            currentOperator = button.value;
+            displayValue = operate(firstValue,secondValue,lastOperator);
+            calculatorDisplay.textContent = displayValue
+        }
+        if(!firstValue){
+            firstValue = Number(calculatorDisplay.textContent)
+        }else if(currentOperator && lastOperator){
+            secondValue = Number(calculatorDisplay.textContent)
+            displayValue = operate(firstValue,secondValue,currentOperator)
+            firstValue = displayValue
+            calculatorDisplay.textContent = displayValue
+        }
+        
+    })
+}
+
+buttonDecimal = document.querySelector('.button-decimal')
+buttonDecimal.addEventListener('click', () =>{
+    if(decimalOff){
+
+    }
+})
