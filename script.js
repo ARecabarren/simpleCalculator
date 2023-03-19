2755b2679522997c14bd7321403dc8350aa78076
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-03-19 14:51:47 -0300

    Fix decimal number operations

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ clearButton.addEventListener('click', () => {
 
 
 let currentOperator,
-    decimalOff = true,
+    decimalOn = false,
     firstValue,
     secondValue,
     displayValue = '';
@@ -68,7 +68,7 @@ numbersButtons = document.querySelectorAll('.button-number');
 for (const button of numbersButtons) {
     button.addEventListener('click', () => {
         if (currentOperator === '/' && button.value === '0') {
-            calculatorDisplay.textContent =  "Paradox alert! \u{1F914}"
+            calculatorDisplay.textContent = "Paradox alert! \u{1F914}"
         } else if (displayValue && currentOperator) {
             if (!secondValue) {
                 secondValue = button.value
@@ -97,14 +97,19 @@ for (const button of operatorsButtons) {
         // There is a previous operator?
         if (firstValue && secondValue) {
             // operar
+
             displayValue = operate(firstValue, secondValue, currentOperator) //Operate and display
             display(displayValue)
             firstValue = displayValue; // Update first value
             secondValue = ''
             currentOperator = button.value //Update current operator
+            if(decimalOn){
+                decimalOn = false
+            }
         } else if (firstValue) {
             //store operator
             currentOperator = button.value
+            decimalOn = false
         } else {
             display('NaN')
             //Display NaN
@@ -121,12 +126,40 @@ buttonEquals.addEventListener('click', () => {
         firstValue = displayValue;
         secondValue = ''
         currentOperator = ''
+        decimalOn = false
     }
 })
 
 buttonDecimal = document.querySelector('.button-decimal')
 buttonDecimal.addEventListener('click', () => {
-    if (decimalOff) {
 
+    if (displayValue === '' || calculatorDisplay.textContent == '0' || calculatorDisplay.textContent == '' ) {
+        decimalOn = true
+        displayValue = '0.'
+        if(secondValue){
+            secondValue = displayValue
+        }else{
+            firstValue = displayValue
+        }
+        display(displayValue)
+    } else if (!decimalOn && secondValue && firstValue && calculatorDisplay.textContent == firstValue)  {
+        decimalOn = true
+        displayValue = '0.'
+        secondValue = displayValue
+        display(displayValue)
+    } else if (!decimalOn && secondValue) {
+        decimalOn = true
+        secondValue += buttonDecimal.value
+        displayValue = secondValue
+        display(secondValue)
+    } else if (decimalOn && firstValue && calculatorDisplay.textContent !== '0.') {
+        decimalOn = true
+        firstValue += buttonDecimal.value
+        displayValue = firstValue
+        display(firstValue)
+    } else{
+        displayValue = '0.'
+        secondValue = displayValue
+        display(displayValue)
     }
 })
