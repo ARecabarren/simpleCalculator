bc4fe1157dfb5b6e7b01a7299c89dfb9bcb4f711
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-03-19 13:01:36 -0300

    Add division by cero management

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // Basic functions
 
-function add(a,b){
-    return a+b
+function add(a, b) {
+    return a + b
 }
-function subtract(a,b) {
-    return a-b
+function subtract(a, b) {
+    return a - b
 }
-function multiply(a,b) {
-    return a*b
+function multiply(a, b) {
+    return a * b
 }
-function divide(a,b){
-    return a/b
+function divide(a, b) {
+    return a / b
 }
-function operate(a,b,operator) {
+function operate(a, b, operator) {
     let result;
     a = Number(a)
     b = Number(b)
-    switch(operator){
+    switch (operator) {
         case '+':
-            result = add(a,b);
+            result = add(a, b);
             break;
         case '-':
-            result = subtract(a,b);
+            result = subtract(a, b);
             break;
         case '*':
-            result = multiply(a,b);
+            result = multiply(a, b);
             break;
         case '/':
-            result = divide(a,b);
+            result = divide(a, b);
             break;
         default:
             result = 'Invalid operator';
@@ -35,15 +35,20 @@ function operate(a,b,operator) {
     } return result
 }
 // Other functions
-function display(value){
-    calculatorDisplay.textContent = value
+function display(value) {
+    if (value.toString().length > 15) {
+        calculatorDisplay.textContent = parseFloat(value).toExponential(5);
+    } else {
+        calculatorDisplay.textContent = value
+
+    }
 }
 
 
 let calculatorDisplay = document.querySelector('.calculator-display')
 
 let clearButton = document.querySelector('.button-clear');
-clearButton.addEventListener('click',() => {
+clearButton.addEventListener('click', () => {
     calculatorDisplay.textContent = "";
     displayValue = ''
     firstValue = undefined;
@@ -61,54 +66,57 @@ let currentOperator,
 numbersButtons = document.querySelectorAll('.button-number');
 
 for (const button of numbersButtons) {
-    button.addEventListener('click', ()=>{
-        if(displayValue && currentOperator){
-            if(!secondValue){
+    button.addEventListener('click', () => {
+        if (currentOperator === '/' && button.value === '0') {
+            calculatorDisplay.textContent =  "Paradox alert! \u{1F914}"
+        } else if (displayValue && currentOperator) {
+            if (!secondValue) {
                 secondValue = button.value
                 display(secondValue)
-            }else{
+            } else {
                 secondValue += button.value
                 display(secondValue)
             }
-        }else if(displayValue){
+        } else if (displayValue) {
             firstValue += button.value
             displayValue += button.value;
             display(displayValue)
-        }else{
+        } else {
             firstValue = button.value
             displayValue += button.value;
             display(displayValue)
         }
+
 
     })
 }
 
 operatorsButtons = document.querySelectorAll('.button-operator')
 for (const button of operatorsButtons) {
-    button.addEventListener('click', () =>{
+    button.addEventListener('click', () => {
         // There is a previous operator?
-        if(firstValue && secondValue ){
+        if (firstValue && secondValue) {
             // operar
-            displayValue = operate(firstValue,secondValue,currentOperator) //Operate and display
+            displayValue = operate(firstValue, secondValue, currentOperator) //Operate and display
             display(displayValue)
             firstValue = displayValue; // Update first value
             secondValue = ''
             currentOperator = button.value //Update current operator
-        }else if(firstValue){
+        } else if (firstValue) {
             //store operator
             currentOperator = button.value
-        }else{
+        } else {
             display('NaN')
             //Display NaN
         }
-        
+
     })
 }
 
 buttonEquals = document.querySelector(".button-equals")
-buttonEquals.addEventListener('click',() =>{
-    if(firstValue,secondValue,currentOperator){
-        displayValue = operate(firstValue,secondValue,currentOperator)
+buttonEquals.addEventListener('click', () => {
+    if (firstValue, secondValue, currentOperator) {
+        displayValue = operate(firstValue, secondValue, currentOperator)
         display(displayValue)
         firstValue = displayValue;
         secondValue = ''
@@ -117,8 +125,8 @@ buttonEquals.addEventListener('click',() =>{
 })
 
 buttonDecimal = document.querySelector('.button-decimal')
-buttonDecimal.addEventListener('click', () =>{
-    if(decimalOff){
+buttonDecimal.addEventListener('click', () => {
+    if (decimalOff) {
 
     }
 })
